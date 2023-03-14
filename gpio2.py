f0a0513cab14595401325bafd528382ab217deb9
thomaskaufi <thomas@kaufmanas.dk>
2023-03-14 09:47:44 +0100

    Update gpio2.py

--- gpio2.py
+++ gpio2.py
@@ -3,11 +3,17 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD) # set GPIO to use the pin numbers instead of pin names
 
-led_pin1 = 7 # pin number 7
-led_pin2 = 11 # pin number 11
+led_pin1 = 35 # pin number 7
+led_pin2 = 36 # pin number 11
+
+led_pin3 = 37 # pin number 7
+led_pin4 = 38 # pin number 11
 
 GPIO.setup(led_pin1,GPIO.OUT) # set the pin to output
 GPIO.setup(led_pin2,GPIO.OUT) # set the pin to output
+
+GPIO.setup(led_pin3,GPIO.OUT) # set the pin to output
+GPIO.setup(led_pin4,GPIO.OUT) # set the pin to output
 
 state = 0
 
@@ -15,8 +21,10 @@ try:
 	print ("Look at the bicolor LED. It should be alternating. Ctrl+C to end.")
 	while(1):
 		GPIO.output(led_pin1,state)
-		GPIO.output(led_pin2,not state)
+		GPIO.output(led_pin2,state)
+		GPIO.output(led_pin3,not state)
+		GPIO.output(led_pin4,not state)
 		state = not state # reverse state
-		sleep(.25) # wait a quarter second
+		sleep(.50) # wait a quarter second
 except KeyboardInterrupt:
 	GPIO.cleanup()
